b8fda65459086cd6781918b3490e3331cba5f429
e2encrypted <100041840+e2encrypted@users.noreply.github.com>
2023-03-26 09:19:26 -0700

    Update ocspcli.sh

--- ocspcli.sh
+++ ocspcli.sh
@@ -16,25 +16,25 @@ fi
 HOST=$1
 PORT=${2:-"443"}	# default port to 443
 
-CERT_FILE=certificate.pem
-ISSUER_CERT_FILE=issuer_certificate.pem
+CERT_FILE="certificate-$HOST.pem"
+ISSUER_CERT_FILE="issuer_certificate-$HOST.pem"
 
 # Get leaf certificate
-echo -n | openssl s_client -connect $HOST:$PORT -servername $HOST 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > $CERT_FILE
+echo -n | openssl s_client -connect $HOST:$PORT -servername $HOST 2>&1 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > "$CERT_FILE"
 
 # Get issuer certificate
 echo -n | openssl s_client -connect $HOST:$PORT -servername -servername -showcerts 2>&1 |
 	sed -ne '/-----BEGIN/,/-----END/p' |
-	perl -0777 -pe 's/.*?-{5}END\sCERTIFICATE-{5}\n//s' > $ISSUER_CERT_FILE
+	perl -0777 -pe 's/.*?-{5}END\sCERTIFICATE-{5}\n//s' > "$ISSUER_CERT_FILE"
 
-OCSP_URL=`openssl x509 -noout -ocsp_uri -in $CERT_FILE`
+OCSP_URL=`openssl x509 -noout -ocsp_uri -in "$CERT_FILE"`
 OCSP_HOST=`echo $OCSP_URL | awk -F/ '{print $3}'`
 
 # Send a query to an OCSP responder with URL $OCSP_URL save the response to a file, print it out in text form, and verify the response:
-openssl ocsp -issuer $ISSUER_CERT_FILE \
-	-cert $CERT_FILE \
+openssl ocsp -issuer "$ISSUER_CERT_FILE" \
+	-cert "$CERT_FILE" \
 	-url $OCSP_URL \
 	-header "HOST" $OCSP_HOST \
-	-VAfile $ISSUER_CERT_FILE \
+	-VAfile "$ISSUER_CERT_FILE" \
 	-resp_text \
 	-respout resp.der
